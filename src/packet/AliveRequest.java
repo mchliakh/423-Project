6705621c29ab6f424cc73e15d1347a28fa3b3f5a
Robert Jakubowicz <robert@robert-jakubowicz.com>
2012-12-12 01:57:34 -0500

    added generated version

--- src/packet/AliveRequest.java
+++ src/packet/AliveRequest.java
@@ -1,6 +1,9 @@
 package packet;
 
 public class AliveRequest extends BasicPacket {
+
+	private static final long serialVersionUID = 8889129756127565302L;
+	
 	private int id;
 	
 	public AliveRequest() {
