666d6004d4ba1c54bd7f39442b5c4acac009b9c7
Robert Jakubowicz <r_jakubo@hidatsa.encs.concordia.ca>
2012-12-12 14:34:28 -0500

    New setup for remote

--- src/app/server/Config.java
+++ src/app/server/Config.java
@@ -10,15 +10,16 @@ public class Config {
 	public static int IM_ALIVE_PORT = 4448; //Patch
 	public static int IM_ALIVE_PORT2 = IM_ALIVE_PORT + 1; //Patch
 	
-//	public static String FRONT_END_NAME = "taos";
-//	public static String SLAVE1_NAME = "tewa";
-//	public static String SLAVE2_NAME = "tano";
-//	public static String LEADER_NAME = "taos";
+	public static String FRONT_END_NAME = "hoopa";
+	public static String SLAVE1_NAME = "haisla";
+	public static String SLAVE2_NAME = "hidatsa";
+	public static String LEADER_NAME = "hoopa";
 
-	public static String FRONT_END_NAME = "localhost";
-	//public static String SLAVE1_NAME = "tewa";
-	public static String SLAVE2_NAME = "localhost";
-	public static String LEADER_NAME = "localhost";
+//	public static String FRONT_END_NAME = "localhost";
+//	//public static String SLAVE1_NAME = "tewa";
+//	public static String SLAVE2_NAME = "localhost";
+//	public static String LEADER_NAME = "localhost";
+//	
 	public static boolean IS_DEBUG = true;
 	public static String DATA_FOLDER = System.getProperty( "user.home" ) + "/workspace/data/";
 }
